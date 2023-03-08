e64b9f108094c9ad1823db311d25a61d1ce7f92e
Yahi <youcefyahi1994@gmail.com>
2023-03-08 18:17:14 +0100

    ajout logo et css navbar

--- la-maison-jungle/src/app/components/navbar.tsx
+++ la-maison-jungle/src/app/components/navbar.tsx
@@ -1,4 +1,9 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import menu from 'public/icons8-menu-arrondi-60.png'
+import logo from 'public/leaf+1.png'
+import close from 'public/icons8-multiplier-30.png'
+
 
 
 export default function Navbar() {
@@ -6,19 +11,22 @@ export default function Navbar() {
         <nav>
             <div className='container'>
                 <ul className="nav-first">
-                    <li>ON verra</li>
-                    <li><p>ICI METTRE UN LOGO RECHERCHER LOGO</p></li>
+                    <li><Image src={menu} alt="test" width={30} height={30}/></li>
+                    <li><h1>MAISON JUNGLE<Image src={logo} alt="test" width={30} height={30}/></h1></li>
                     <div className="nav-information">
-                        <li><Link href="/">Connexion</Link></li>
+                        <li className='nav-connexion'><Link href="/">Connexion</Link></li>
                         <li><Link href="/">Panier</Link></li>
                     </div>
 
                 </ul>
                 <div className='nav-border'></div>
-                <ul className="nav-secondary">
+                <ul className="nav-secondary open">
+                    <li><Image src={close} alt="test"/></li>
                     <li><Link href="/">Plante intérieur</Link></li>
                     <li><Link href="/">Terarium</Link></li>
                     <li><Link href="/">Kokedama</Link></li>
+                    <li className='nav__secondary-connexion'><Link href="/">Connexion</Link></li>
+                  
                 </ul>
             </div>
         </nav>
