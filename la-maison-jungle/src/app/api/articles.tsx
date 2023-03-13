0fff901dd2cc00cc84a1c6684c110cfef6137486
Yahi <youcefyahi1994@gmail.com>
2023-03-13 19:06:56 +0100

    css single article

--- la-maison-jungle/src/app/api/articles.tsx
+++ la-maison-jungle/src/app/api/articles.tsx
@@ -6,35 +6,35 @@ const article: Articles[] = [
         id: 1,
         name: "Strelitzia nicolai XL",
         price:199,
-        description: "string",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nunc est, faucibus id mi sed, gravida rhoncus est. Ut dapibus suscipit lectus, eu porttitor tellus. Etiam tellus sapien, commodo at mattis laoreet, dignissim quis eros. Duis vulputate lacus urna, in porttitor elit scelerisque id. Praesent cursus quis mi vel elementum. Mauris commodo pulvinar leo et auctor. Vestibulum quam odio, rutrum non lacus sed, faucibus tempor ipsum.",
         image:'https://cdn.shopify.com/s/files/1/0004/2654/1108/products/livraison_grande_plante_strelitziaV2_1300x.jpg?v=1677830075'
     },
     {
         id: 2,
         name: "Strelitzia nicolai XL",
         price:199,
-        description: "string",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nunc est, faucibus id mi sed, gravida rhoncus est. Ut dapibus suscipit lectus, eu porttitor tellus. Etiam tellus sapien, commodo at mattis laoreet, dignissim quis eros. Duis vulputate lacus urna, in porttitor elit scelerisque id. Praesent cursus quis mi vel elementum. Mauris commodo pulvinar leo et auctor. Vestibulum quam odio, rutrum non lacus sed, faucibus tempor ipsum.",
         image:'https://img.grouponcdn.com/deal/3P4Yw2D1kb9qF7V3VjAwbqdAywqT/3P-807x484/v1/t600x362.jpg'
     },
     {
         id: 3,
         name: "Strelitzia nicolai XL",
         price:199,
-        description: "string",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nunc est, faucibus id mi sed, gravida rhoncus est. Ut dapibus suscipit lectus, eu porttitor tellus. Etiam tellus sapien, commodo at mattis laoreet, dignissim quis eros. Duis vulputate lacus urna, in porttitor elit scelerisque id. Praesent cursus quis mi vel elementum. Mauris commodo pulvinar leo et auctor. Vestibulum quam odio, rutrum non lacus sed, faucibus tempor ipsum.",
         image:'https://cdn.shopify.com/s/files/1/0004/2654/1108/products/livraison_grande_plante_strelitziaV2_1300x.jpg?v=1677830075'
     },
     {
         id: 4,
         name: "Strelitzia nicolai XL",
         price:199,
-        description: "string",
+        description: "",
         image:'https://cdn.shopify.com/s/files/1/0004/2654/1108/products/livraison_grande_plante_strelitziaV2_1300x.jpg?v=1677830075'
     },
     {
         id: 5,
         name: "Strelitzia nicolai XL",
         price:199,
-        description: "string",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nunc est, faucibus id mi sed, gravida rhoncus est. Ut dapibus suscipit lectus, eu porttitor tellus. Etiam tellus sapien, commodo at mattis laoreet, dignissim quis eros. Duis vulputate lacus urna, in porttitor elit scelerisque id. Praesent cursus quis mi vel elementum. Mauris commodo pulvinar leo et auctor. Vestibulum quam odio, rutrum non lacus sed, faucibus tempor ipsum.",
         image:'https://cdn.shopify.com/s/files/1/0004/2654/1108/products/livraison_grande_plante_strelitziaV2_1300x.jpg?v=1677830075'
     }
  ]
