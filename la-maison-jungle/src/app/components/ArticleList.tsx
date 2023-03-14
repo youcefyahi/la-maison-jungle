92116a5e62d590cc70dec5942e6c25dddaf3fbee
Yahi <youcefyahi1994@gmail.com>
2023-03-14 19:13:39 +0100

    debut css listarticle

--- la-maison-jungle/src/app/components/ArticleList.tsx
+++ la-maison-jungle/src/app/components/ArticleList.tsx
@@ -8,11 +8,10 @@ export const ArticlesList: React.FC<> = ({ }) => {
 
     return (
         <div className='articles-list'>
-            <Link href="">
-                <img src="" alt='mettre image ici plus tard la faire border' />
-                <Link href="/">{article[0].name}</Link>
-                <p>{article[0].price}</p>
-            </Link>
+            <Link href="/">
+                <img src="" alt='mettre image ici plus tard la faire border' /> {article[0].name}</Link>
+            <p>{article[0].price}ê</p>
+
         </div>
 
     )
