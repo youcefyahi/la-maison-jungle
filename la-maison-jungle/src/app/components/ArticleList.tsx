3675f4ce76640a56b57ee80a970cbda32d760aac
Yahi <youcefyahi1994@gmail.com>
2023-03-17 12:45:36 +0100

    mise en place css (non responsive)

--- la-maison-jungle/src/app/components/ArticleList.tsx
+++ la-maison-jungle/src/app/components/ArticleList.tsx
@@ -1,6 +1,9 @@
 import '../styles/articlesList.scss'
 import Link from 'next/link'
 import article from '../api/articles'
+import truc from "../assets/areca.jpeg"
+import Image from 'next/image'
+
 
 
 
@@ -9,7 +12,7 @@ export const ArticlesList: React.FC<> = ({ }) => {
     return (
         <div className='articles-list'>
             <Link href="/">
-                <img src="" alt='mettre image ici plus tard la faire border' /> {article[0].name}</Link>
+                <Image src={truc} alt='mettre image ici plus tard la faire border' /> <p>{article[0].name}</p></Link>
             <p>{article[0].price}ê</p>
 
         </div>
